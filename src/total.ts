8ff18d9a6db96157c8d9ab1daafca4897b54f283
msanchez-2025319 <msanchez-2025319@kinal.edu.gt>
2026-06-19 22:10:33 -0600

    docs: se agregó documentación interna y se corrigieron funciones

--- src/total.ts
+++ src/total.ts
@@ -1,3 +1,16 @@
 export const totalTodo = (subToTal: number, cuatoEsDeIva: number) =>{
     return subToTal + cuatoEsDeIva;
 }
+/**
+ * PARAMETROS QUE RESIVE
+ * subtotal (number) este parametro que resive el subtotal(la suma de 
+ * las cantidades que obtiene el subtotal) cuantoEsDeIva (number) 
+ * este parametro risive el iva que se le saco a el subtotal.
+ * 
+ * DEVUELVE
+ * El total devuelve lo que es las suma de subtotal más iva = total.
+ * 
+ * FUNCION
+ * Esta funcion es la que me permite saber cuanto necesito pagar en 
+ * total más iva.  
+ */
